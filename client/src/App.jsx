952b80743b2a2a70b992368489c2dd8508bc0fc1
suvani <suvaniaher04@gmail.com>
2026-07-13 00:40:11 +0530

    updated in app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl font-normal bg-yellow-300 text-red-600">hii im suvnai</h1>
+    <h1 className="text-3xl font-normal  bg-yellow-300 text-green-800">hii im suvnai</h1>
   );
 }
 
